6bdfaf253c9c8ee19af209e39fe66680c48bfdf3
Riadh Mankai <rmankai@gmail.com>
2017-09-01 10:48:05 -0400

    Add "checkBingo" Methods which detected when a bingo is reached on rows, cols or diaglonals

--- force-app/main/default/aura/BingoCmp/BingoCmpHelper.js
+++ force-app/main/default/aura/BingoCmp/BingoCmpHelper.js
@@ -1,6 +1,6 @@
 ({
     buildBingoHlp : function(cmp) {
-        let bingoList = [
+        let lst_bingoRows = [
             ["No It's still loading", "Loud, Painful Echo / Feedback", "I have a hard stop at...", "Can everyone see my screen?", "I'm sorry, I was on mute"], 
             ["Hey can you hear me?", "I'm sorry, you cut out here", "So (faded out) I can (unintelligible) by (fades out) ok?", "I'll have to get back to you", "Can everyone go on mute?"], 
             ["Hi, who joined?", "Uhhh, are you still there?", "(Child or animal noises)?", "Hi guys, I have to jump to another call", "Hello, HELLO, HELLO ??!!"],
@@ -8,62 +8,88 @@
             ["(Someone is eating or brushing her teeth)", "Sorry, come again!", "I think there is a bug", "Sorry, I am having a connection issues", "Can you go on Michelin Skype?"]];
 
         // Init all cells to "Unselected"
-        for (let i = 0; i < bingoList.length; i++) {
-            bingoList[i] = this.unselectAllCells(bingoList[i]);
-        }
-        cmp.set("v.bingoList", bingoList);
+        this.initBingoBoard(lst_bingoRows);
     },
 
     selectCellHlp : function(cmp, event) {
         let cell = event.currentTarget;
-        let colIndex = cell.getAttribute("data-colIndex");
         let rowIndex = cell.getAttribute("data-rowIndex");
-        let bingoList = cmp.get("v.bingoList");
+        let colIndex = cell.getAttribute("data-colIndex");
+        let lst_bingoRows = cmp.get("v.bingoRows");
 
-        if (!bingoList[rowIndex][colIndex].isSelected) {
-            bingoList[rowIndex][colIndex].isSelected = true;
+        if (!lst_bingoRows[rowIndex][colIndex].isSelected) {
+            lst_bingoRows[rowIndex][colIndex].isSelected = true;
             $A.util.addClass(cell, 'selectedBg');
         }
         else {
-            bingoList[rowIndex][colIndex].isSelected = false;
+            lst_bingoRows[rowIndex][colIndex].isSelected = false;
             $A.util.removeClass(cell, 'selectedBg');
         }
 
-        cmp.set("v.bingoList", bingoList);
+        cmp.set("v.bingoRows", lst_bingoRows);
+        // Verify if Bingo is reached, aka a whole row, column or diagonal is selected
         this.checkBingo(cmp);
     },
 
-    unselectAllCells: function(p_bingoList) {
-        let bingoWrappers = [];
-        for (let i = 0; i < p_bingoList.length; i++) {
-            let wrapper = {
-                value : p_bingoList[i],
-                isSelected : false
-            };
-            bingoWrappers.push(wrapper);
+    initBingoBoard: function(p_bingoRows) {
+        let lst_bingoWrappers = [];
+        let bingoSize = p_bingoRows.length;
+
+        for (let i = 0; i < bingoSize; i++) {
+            let lst_bingoRowWrappers = [];
+            for (let j = 0; j < bingoSize; j++) {
+                let rowWrapper = {
+                    cellValue : p_bingoRows[i][j],
+                    isSelected : false
+                };
+                lst_bingoRowWrappers.push(rowWrapper);
+            }
+            lst_bingoWrappers.push(lst_bingoRowWrappers);
         }
-        return bingoWrappers;
+        cmp.set("v.bingoRows", lst_bingoWrappers);
     },
 
+    // The methods checks if we reached a Bingo. A Bingo is reached when a whole
+    // row, column or diagonal is selected
     checkBingo: function(cmp) {
-        let bingoList = cmp.get("v.bingoList");
+        let lst_bingoRows = cmp.get("v.bingoRows");
+        // Size of the array - Bingo board is always a square
+        let bingoSize = lst_bingoRows.length;
+        let bingoDiagonalExists = true;
+        let bingoInverseDiagonalExists = true;
 
-        for (let i = 0; i < bingoList.length; i++) {
-            let bingoExists = true;
-            let row = bingoList[i];
-            for (let j = 0; j < row.length; j++) {
-                if (!row[j].isSelected) {
-                    bingoExists = false;
-                    break;
+        for (let i = 0; i < bingoSize; i++) {
+            let bingoRowExists = true;
+            let bingoColExists = true;
+            for (let j = 0; j < bingoSize; j++) {
+                if (!lst_bingoRows[i][j].isSelected) {
+                    bingoRowExists = false;
+                }
+                if (!lst_bingoRows[j][i].isSelected) {
+                    bingoColExists = false;
                 }
             }
-            if (bingoExists) {
-                console.log("bigno !");
+
+            if (!lst_bingoRows[i][i].isSelected) {
+                bingoDiagonalExists = false;
+            }
+            if (!lst_bingoRows[i][bingoSize -i -1].isSelected) {
+                bingoInverseDiagonalExists = false;
+            }
+
+            if (bingoRowExists || bingoColExists) {
+                console.log("bigno row or col !");
                 // Init the bingo board
-                this.buildBingoHlp(cmp); 
-                break;
+                this.initBingoBoard(lst_bingoRows);
+                return;
             }
         }
+        if (bingoDiagonalExists || bingoInverseDiagonalExists) {
+            console.log("bigno diag !");
+            this.initBingoBoard(lst_bingoRows);
+            return;
+        }
+
     }
 
 })
